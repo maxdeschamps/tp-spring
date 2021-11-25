cd681c9cee63150dcce016841d453c5b5d13147f
Maxence Deschamps <maxence.deschamps.pro@gmail.com>
2021-11-25 10:52:29 +0100

    ajout des routes classiques du projet

--- src/main/java/com/tpspring/controllers/ProjetController.java
+++ src/main/java/com/tpspring/controllers/ProjetController.java
@@ -1,23 +1,39 @@
 package com.tpspring.controllers;
 
+import com.tpspring.entities.Projet;
 import com.tpspring.services.*;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class ProjetController {
     @Autowired
     private ProjetService projetService;
-    @Autowired
-    private MessageService messageService;
-    @Autowired
-    private UtilisateurService utilisateurService;
-    @Autowired
-    private PatientService patientService;
-    @Autowired
-    private RequeteService requeteService;
-    @Autowired
-    private ServiceService serviceService;
-    @Autowired
-    private MotCleService motCleService;
+
+    @RequestMapping(path = "/projet", method = RequestMethod.GET)
+    public Projet getProjet(@RequestParam(value = "id") Integer id) {
+        return projetService.getProjetById(id);
+    }
+
+    @Operation(summary = "Création ou mise à jour d'un utilisateur")
+    @RequestMapping(path = "/projet", method = RequestMethod.PUT)
+    public Projet addOrUpdateProjet(@Valid @RequestBody Projet projet) {
+        return projetService.createOrUpdate(projet);
+    }
+
+    @Operation(summary = "Récupération de tous les utilisateurs")
+    @RequestMapping(path = "/projets/all", method = RequestMethod.GET)
+    public List<Projet> getProjets() {
+        return projetService.getAllProjets();
+    }
+
+    @Operation(summary = "Suppression d'un utilisateur à partir de son identifiant")
+    @RequestMapping(path = "/projet", method = RequestMethod.DELETE)
+    public void deleteProjet(@RequestParam(value = "id") Integer id) {
+        projetService.deleteProjet(projetService.getProjetById(id));
+    }
 }
--- src/main/java/com/tpspring/repositories/ProjetRepository.java
+++ src/main/java/com/tpspring/repositories/ProjetRepository.java
@@ -1,11 +1,10 @@
 package com.tpspring.repositories;
 
+import com.tpspring.entities.Projet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface ProjetRepository extends JpaRepository<ProjetRepository, Integer> {
+public interface ProjetRepository extends JpaRepository<Projet, Integer> {
 
 }
