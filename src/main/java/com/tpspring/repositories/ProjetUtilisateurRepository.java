a48161b26f3c732d35a4e2c8adec353c438a63de
Jean-Philippe Bourdais <jeanphi.bourdais@gmail.com>
2021-11-26 12:40:42 +0100

    add method

--- src/main/java/com/tpspring/repositories/ProjetUtilisateurRepository.java
+++ src/main/java/com/tpspring/repositories/ProjetUtilisateurRepository.java
@@ -1,10 +1,16 @@
 package com.tpspring.repositories;
 
+import com.tpspring.entities.Projet;
 import com.tpspring.entities.ProjetUtilisateur;
+import com.tpspring.entities.Requete;
+import com.tpspring.entities.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjetUtilisateurRepository extends JpaRepository<ProjetUtilisateur, Integer> {
 
+    @Query("select pu from ProjetUtilisateur pu where pu.projet = ?1 and pu.utilisateur = ?2")
+    Requete findParticipationByProjetAndUtilisateur(Integer projetId, Integer utilisateurId);
 }
